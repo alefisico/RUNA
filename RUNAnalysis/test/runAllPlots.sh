c97bc67652e82c1730ea3ab4e35feeaf028533e2
Alejandro <gomez@physics.rutgers.edu>
2017-05-26 23:16:47 +0200

    updating;

---
+++ RUNAnalysis/test/runAllPlots.sh
@@ -0,0 +1,19 @@
+./DrawHistogram.py -p qual -t -s HT -C _cutBestPair -b Resolved -v v07p1 -q Pt -l 1900 
+./DrawHistogram.py -p qual -t -s HT -C _cutBestPair -b Resolved -v v07p1 -q Pt -l 1900 -e pdf
+./DrawHistogram.py -p Norm -s massAsym -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved
+./DrawHistogram.py -p Norm -s massAsym -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+./DrawHistogram.py -p Norm -s deltaEta -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved
+./DrawHistogram.py -p Norm -s deltaEta -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+./DrawHistogram.py -p 2D -s deltavsMassAve -C _cutBestPair -m 500 -t -v v07p1 -q Pt -b Resolved -l 1900
+./DrawHistogram.py -p 2D -s deltavsMassAve -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -l 1900
+./DrawHistogram.py -p 2D -s deltavsMassAve -C _cutBestPair -m 500 -t -v v07p1 -q Pt -b Resolved -l 1900 -e pdf
+./DrawHistogram.py -p 2D -s deltavsMassAve -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -l 1900 -e pdf
+./DrawHistogram.py -p qual -s massAsym -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved
+./DrawHistogram.py -p qual -s massAsym -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+./DrawHistogram.py -p qual -s deltaEta -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved
+./DrawHistogram.py -p qual -s deltaEta -C _cutBestPair -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+./DrawHistogram.py -p Norm -s deltaEta -C _n-1 -m 300 -t -v v07p1 -q Pt -b Resolved 
+./DrawHistogram.py -p Norm -s deltaEta -C _n-1 -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+./DrawHistogram.py -p Norm -s massAsym -C _n-1 -m 300 -t -v v07p1 -q Pt -b Resolved 
+./DrawHistogram.py -p Norm -s massAsym -C _n-1 -m 300 -t -v v07p1 -q Pt -b Resolved -e pdf
+
